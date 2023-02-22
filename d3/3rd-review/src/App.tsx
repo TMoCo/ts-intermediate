6c07d3183f1200184ded2da9ed7ce8b976dc651b
Thomas Moreno Cooper <thomas.cooper@bbc.co.uk>
2023-02-22 16:47:12 +0000

    custom observable hook

---
+++ d3/3rd-review/src/App.tsx
@@ -0,0 +1,15 @@
+import './App.css';
+import SearchBar from './SearchBar';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>Enter a swapi category</p>
+        <SearchBar></SearchBar>
+      </header>
+    </div>
+  );
+}
+
+export default App;
